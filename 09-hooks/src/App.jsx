e7957520ef747206ede3cc05e7717684b7c259ff
Monu Singh <monusingh995606@gmail.com>
2026-01-16 00:51:52 +0530

    usestate complete

--- 09-hooks/src/App.jsx
+++ 09-hooks/src/App.jsx
@@ -1,20 +1,47 @@
 
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
+
 
 function App() {
 
-let a = 20;
-function changeA(){
- console.log(a)
-  a=30
-  console.log(a)
-}
+  // const [num, setnum] = useState(0)
+
+  const [num, setnum] = useState({user:"Monu" , age:20})
+
+  const btnClicked = ()=>{
+    // const Newuser = {...num}
+    // Newuser.user = "Aman"
+    // Newuser.age = 21
+    // setnum(Newuser)
+
+    setnum(prev =>({...prev,age:50}))
+  }
+
+
+
+
+
+  // const [num, setnum] = useState([10,20,30])
+
+  //   const btnClicked = ()=>{
+  //   const newNum = [...num] 
+  //   newNum.push(99)
+  //   console.log(newNum)
+  //   setnum(newNum)
+  // }
   return (
    
     <div>
-     <h1>value of a is {a}</h1>
-     <button onClick={changeA}>Click</button>
+    {/* <h1>{num}</h1> */}
+    {/* <button onClick={()=>setnum(num+1)}>Increase</button>
+    <button onClick={()=>setnum(num-1)}>Decrease</button>
+    <button onClick={()=>setnum(num+5)}>Jump by 5</button> */}
+
+    <h1>{num.user} , {num.age}</h1>
+    <button onClick={btnClicked}>click</button>
+
+    {/* <h1>{num}</h1>
+    <button onClick={btnClicked}>click</button>  */}
     </div>
   )
 }
